f2110e521bc514333b2d2612d0e143774f4c9ba7
Kulsoom Khurshid <kulsoomkhurshid26@gmail.com>
2025-09-08 00:01:14 +0400

    Committing: done

--- lib/utils/screen_bindings.dart
+++ lib/utils/screen_bindings.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 import 'package:viria_dashboard/screens/settings_screen/controller/settings_controller.dart';
+import 'package:viria_dashboard/screens/subcription_screen/controller/subscription_controller.dart';
+import 'package:viria_dashboard/screens/sidemenu/controller/sidemenu_controller.dart';
 import 'package:viria_dashboard/screens/subtitle_screen/controller/subtitle_controller.dart';
 import 'package:viria_dashboard/screens/user_screen/controller/user_controller.dart';
 import 'package:viria_dashboard/screens/video_screen/controller/video_controller.dart';
@@ -10,6 +12,8 @@ class ScreenBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => DashboardController());
+    Get.lazyPut(() => UserController());
+    Get.lazyPut(() => SideMenuController());
     Get.lazyPut(() => SettingsController());
     Get.lazyPut<SubtitleController>(() => SubtitleController(), fenix: true);
     Get.lazyPut(() => VideoController());
